18c7ca787bdc7faa5338684d90cc1ec24990250b
Andy Yo <andyyo@Andys-MacBook-Air.local>
2024-11-22 12:14:27 +0800

    Update ChatMessage.java

--- src/main/java/com/andy/enigmask/model/ChatMessage.java
+++ src/main/java/com/andy/enigmask/model/ChatMessage.java
@@ -1,13 +1,22 @@
 package com.andy.enigmask.model;
 
+import jakarta.persistence.*;
 import lombok.Data;
 
+import java.time.LocalDateTime;
+
 @Data
+@Entity
 public class ChatMessage {
-    private String id;             // Unique identifier for the message
-    private String senderId;       // Sender's user ID
-    private String recipientId;    // Recipient's user ID
-    private String content;        // Encrypted content
-    private String timestamp;      // Timestamp of the message
-    private MessageStatus status;  // Status of the message (PENDING, SENT, DELIVERED, READ)
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;                      // Unique identifier for the message
+    private String senderId;              // Sender's user ID
+    private String recipientId;           // Recipient's user ID
+    private String content;               // Encrypted content
+    private LocalDateTime timestamp;      // Timestamp of the message
+
+    @Enumerated(EnumType.STRING)
+    private MessageStatus status;         // Status of the message (PENDING, SENT, DELIVERED, READ)
 }
