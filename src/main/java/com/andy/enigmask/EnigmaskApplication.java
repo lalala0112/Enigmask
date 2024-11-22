ccd21e70e366d9b850a6b9e50d56f3dd909f8203
Andy Yo <andyyo@Andys-MacBook-Air.local>
2024-11-22 08:49:51 +0800

    Add initial dependencies for web application

---
+++ src/main/java/com/andy/enigmask/EnigmaskApplication.java
@@ -0,0 +1,13 @@
+package com.andy.enigmask;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EnigmaskApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EnigmaskApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/andy/enigmask/EnigmaskApplicationTests.java
@@ -0,0 +1,13 @@
+package com.andy.enigmask;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EnigmaskApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
