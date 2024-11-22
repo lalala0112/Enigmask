bfa51239c031e0dda21e9d8ac8073f6acab18bda
Andy Yo <andyyo@Andys-MacBook-Air.local>
2024-11-22 11:44:35 +0800

    Add ChatMessage.java

---
+++ src/main/java/com/andy/enigmask/model/ChatMessage.java
@@ -0,0 +1,13 @@
+package com.andy.enigmask.model;
+
+import lombok.Data;
+
+@Data
+public class ChatMessage {
+    private String id;             // Unique identifier for the message
+    private String senderId;       // Sender's user ID
+    private String recipientId;    // Recipient's user ID
+    private String content;        // Encrypted content
+    private String timestamp;      // Timestamp of the message
+    private MessageStatus status;  // Status of the message (SENT, DELIVERED, READ)
+}
