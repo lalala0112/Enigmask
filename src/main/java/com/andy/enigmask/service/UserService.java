80f9bdfd1e6909c9c338cc9f9f62bf6b562ad844
Andy Yo <andyyo@Andys-MacBook-Air.local>
2024-11-24 11:05:35 +0800

    Add UserService.java

---
+++ src/main/java/com/andy/enigmask/service/UserService.java
@@ -0,0 +1,37 @@
+package com.andy.enigmask.service;
+
+import com.andy.enigmask.repository.UserRepository;
+import com.andy.enigmask.user.Status;
+import com.andy.enigmask.user.User;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+@RequiredArgsConstructor
+public class UserService {
+
+    private final UserRepository userRepository;
+
+    public void saveUser(User user) {
+        user.setStatus(Status.ONLINE);
+        userRepository.save(user);
+    }
+
+    public void disconnectUser(User user) {
+        var storedUser = userRepository
+                .findById(user.getUsername())
+                .orElse(null);
+
+        if (storedUser != null) {
+            storedUser.setStatus(Status.OFFLINE);
+            userRepository.save(storedUser);
+        }
+
+    }
+
+    public List<User> findConnectedUsers() {
+        return userRepository.findAllByStatus(Status.ONLINE);
+    }
+}
