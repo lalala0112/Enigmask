dd612d50081b8d3864f9a4996693acd99e258d9e
Andy Yo <andyyo@Andys-MacBook-Air.local>
2024-11-26 10:51:00 +0800

    Update docker-compose.yml

--- src/main/resources/db/migration/schema.sql
+++
@@ -1,8 +0,0 @@
-CREATE TABLE chat_messages (
-    id VARCHAR(255) PRIMARY KEY,
-    sender_id VARCHAR(255) NOT NULL,
-    recipient_id VARCHAR(255) NOT NULL,
-    content TEXT NOT NULL,
-    status VARCHAR(50),
-    timestamp TIMESTAMP NOT NULL
-);
