3c9829d6f0007fc683a1564ad54f82dbaf57f3db
Andy Yo <andyyo@Andys-MacBook-Air.local>
2024-11-26 13:24:02 +0800

    Update ChatMessageService.java

--- src/main/java/com/andy/enigmask/service/ChatMessageService.java
+++ src/main/java/com/andy/enigmask/service/ChatMessageService.java
@@ -1,53 +1,42 @@
 package com.andy.enigmask.service;
 
 import com.andy.enigmask.model.ChatMessage;
-import com.andy.enigmask.model.MessageStatus;
 import com.andy.enigmask.repository.ChatMessageRepository;
-import org.springframework.transaction.annotation.Transactional;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
 
+import java.util.ArrayList;
 import java.util.List;
 
+@Service
+@RequiredArgsConstructor
 public class ChatMessageService {
 
     private final ChatMessageRepository chatMessageRepository;
-
-    public ChatMessageService(ChatMessageRepository chatMessageRepository) {
-        this.chatMessageRepository = chatMessageRepository;
-    }
+    private final ChatRoomService chatRoomService;
 
     public ChatMessage saveMessage(ChatMessage chatMessage) {
-        return chatMessageRepository.save(chatMessage);
-    }
-
-    public List<ChatMessage> getMessagesBetweenUsers(String senderId, String recipientId) {
-        return chatMessageRepository.findBySenderIdAndRecipientId(senderId, recipientId);
-    }
-
-    public long countUnreadMessages(String senderId, String recipientId) {
-        return chatMessageRepository.countBySenderIdAndRecipientIdAndStatus(
-                senderId, recipientId, MessageStatus.DELIVERED
+        var chatId = chatRoomService.getChatRoomId(
+                chatMessage.getSenderId(),
+                chatMessage.getRecipientId(),
+                true
         );
+
+        chatMessage.setChatId(String.valueOf(chatId));
+        chatMessageRepository.save(chatMessage);
+
+        return chatMessage;
     }
 
-    @Transactional
-    public void markMessagesAsRead(String senderId, String recipientId) {
-        chatMessageRepository.updateStatus(senderId, recipientId, MessageStatus.DELIVERED, MessageStatus.READ);
-    }
-
-    public List<ChatMessage> getUnreadMessages(String senderId, String recipientId) {
-        return chatMessageRepository.findByRecipientIdAndStatus(
-                recipientId, MessageStatus.DELIVERED
+    public List<ChatMessage> findChatMessages(
+            String senderId, String recipientId
+    ) {
+        var chatId = chatRoomService.getChatRoomId(
+                senderId,
+                recipientId,
+                false
         );
+        return chatId.map(chatMessageRepository::findByChatId).orElse(new ArrayList<>());
     }
 
-    public void updateMessageStatus(Long chatMessageId, MessageStatus status) {
-        chatMessageRepository.findById(chatMessageId).ifPresent(message -> {
-            message.setStatus(status);
-            chatMessageRepository.save(message);
-        });
-    }
-
-    public List<ChatMessage> getMessagesByRecipientAndStatus(String recipientId, MessageStatus status) {
-        return chatMessageRepository.findByRecipientIdAndStatus(recipientId, status);
-    }
 }
