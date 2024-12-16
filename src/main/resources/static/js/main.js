8693077b574a5b165407396b033530a593096474
殷郁婷 <lovemusic11024@gmail.com>
2024-12-16 19:45:20 +0800

    Update main.js register

--- src/main/resources/static/js/main.js
+++ src/main/resources/static/js/main.js
@@ -13,15 +13,17 @@ const registerLink = document.querySelector('#register-link');
 const registerPage = document.querySelector('#register-page');
 
 let stompClient = null;
-let nickname = null;
-let fullname = null;
+let username = null;
+let password = null;
 let selectedUserId = null;
+let confirmPassword = null;
 
 function connect(event) {
-    nickname = document.querySelector('#nickname').value.trim();
-    fullname = document.querySelector('#fullname').value.trim();
+    username = document.querySelector('#username').value.trim();
+    password = document.querySelector('#password').value.trim();
 
-    if (nickname && fullname) {
+
+    if (username && password) {
         usernamePage.classList.add('hidden');
         chatPage.classList.remove('hidden');
 
@@ -34,21 +36,21 @@ function connect(event) {
 }
 
 function onConnected() {
-    stompClient.subscribe(`/user/${nickname}/queue/messages`, onMessageReceived);
+    stompClient.subscribe(`/user/${username}/queue/messages`, onMessageReceived);
     stompClient.subscribe(`/user/topic/public`, onMessageReceived);
 
     // register the connected user
     stompClient.send("/app/user.addUser",
         {},
-        JSON.stringify({nickname: nickname, fullname: fullname, status: 'ONLINE'})
+        JSON.stringify({username: username, password: password, status: 'ONLINE'})
     );
 
-    const connectedUserElement = document.querySelector('#connected-user-fullname');
+    const connectedUserElement = document.querySelector('#connected-user-username');
 
     if (connectedUserElement) {
-        connectedUserElement.textContent = fullname;
+        connectedUserElement.textContent = username;
     } else {
-        console.error("Error: The element '#connected-user-fullname' does not exist in the DOM.");
+        console.error("Error: The element '#connected-user-username' does not exist in the DOM.");
     }
 
     findAndDisplayConnectedUsers().then();
@@ -68,7 +70,7 @@ async function findAndDisplayConnectedUsers() {
             connectedUsers = responseData;
         } else if (responseData.users && Array.isArray(responseData.users)) {
             connectedUsers = responseData.users;
-        } else if (responseData.nickName) {
+        } else if (responseData.username) {
             connectedUsers = [responseData];
         } else {
             connectedUsers = [];
@@ -80,7 +82,7 @@ async function findAndDisplayConnectedUsers() {
     }
 
     // Display the connected users
-    const filteredUsers = connectedUsers.filter(user => user.nickName !== nickname);
+    const filteredUsers = connectedUsers.filter(user => user.username !== username);
     const connectedUsersList = document.getElementById('connectedUsers');
     connectedUsersList.innerHTML = '';
 
@@ -97,14 +99,14 @@ async function findAndDisplayConnectedUsers() {
 function appendUserElement(user, connectedUsersList) {
     const listItem = document.createElement('li');
     listItem.classList.add('user-item');
-    listItem.id = user.nickName;
+    listItem.id = user.username;
 
     const userImage = document.createElement('img');
     userImage.src = '../image/user_icon.png';
-    userImage.alt = user.fullName;
+    userImage.alt = user.username;
 
     const usernameSpan = document.createElement('span');
-    usernameSpan.textContent = user.fullName;
+    usernameSpan.textContent = user.username;
 
     const receivedMsgs = document.createElement('span');
     receivedMsgs.textContent = '0';
@@ -140,7 +142,7 @@ function userItemClick(event) {
 function displayMessage(senderId, content) {
     const messageContainer = document.createElement('div');
     messageContainer.classList.add('message');
-    if (senderId === nickname) {
+    if (senderId === username) {
         messageContainer.classList.add('sender');
     } else {
         messageContainer.classList.add('receiver');
@@ -152,7 +154,7 @@ function displayMessage(senderId, content) {
 }
 
 async function fetchAndDisplayUserChat() {
-    const userChatResponse = await fetch(`/messages/${nickname}/${selectedUserId}`);
+    const userChatResponse = await fetch(`/queue/messages/${username}/${selectedUserId}`);
     const userChat = await userChatResponse.json();
     chatArea.innerHTML = '';
 
@@ -173,19 +175,18 @@ function sendMessage(event) {
     const messageContent = messageInput.value.trim();
     if (messageContent && stompClient) {
         const chatMessage = {
-            senderId: nickname,
+            senderId: username,
             recipientId: selectedUserId,
             content: messageInput.value.trim(),
             timestamp: new Date()
         };
         stompClient.send("/app/chat", {}, JSON.stringify(chatMessage));
-        displayMessage(nickname, messageInput.value.trim());
+        displayMessage(username, messageInput.value.trim());
         messageInput.value = '';
     }
     chatArea.scrollTop = chatArea.scrollHeight;
     event.preventDefault();
 }
-
 
 async function onMessageReceived(payload) {
     await findAndDisplayConnectedUsers();
@@ -213,7 +214,7 @@ async function onMessageReceived(payload) {
 function onLogout() {
     stompClient.send("/app/user.disconnectUser",
         {},
-        JSON.stringify({nickName: nickname, fullName: fullname, status: 'OFFLINE'})
+        JSON.stringify({username: username, password: password, status: 'OFFLINE'})
     );
     window.location.reload();
 }
@@ -224,36 +225,33 @@ function jump_reg() {
 }
 
 function register(event) {
-    const nickname = document.querySelector('#r_nickname').value.trim();
-    const password = document.querySelector('#r_fullname').value.trim();
-    const confirmPassword = document.querySelector('#r2_fullname').value.trim();
+    username = document.querySelector('#r_nickname').value.trim();
+    password = document.querySelector('#r_fullname').value.trim();
+    confirmPassword = document.querySelector('#r2_fullname').value.trim();
 
     if (password !== confirmPassword) {
         alert('密碼不一致，請重新輸入。');
         return;
     }
 
-    fetch('/register', {
-        method: 'POST',
-        headers: {
-            'Content-Type': 'application/json'
-        },
-        body: JSON.stringify({ nickname, password })
-    })
-        .then(response => response.json())
-        .then(data => {
-            if (data.success) {
-                alert('註冊成功，請登入。');
-                registerPage.classList.add('hidden');
-                usernamePage.classList.remove('hidden');
-            }
-            else {
-                alert(data.message);
-            }
-        })
-        .catch(error => {
-            console.error('註冊出錯：', error);
+    const socket = new SockJS('/ws');
+    stompClient = Stomp.over(socket);
+
+    // 寫入資料庫
+    stompClient.connect({}, function() {
+        console.log('STOMP 連接成功，發送用戶註冊請求...');
+
+        stompClient.send("/app/user.addUser", {},
+            JSON.stringify({username: username, password: password}));
+
+        alert('註冊成功，請登入。');
+        registerPage.classList.add('hidden');
+        usernamePage.classList.remove('hidden');
+        }, function(error) {
+            console.error('STOMP 連接失敗: ', error);
+            alert('無法連接到伺服器，請稍後再試');
         });
+    event.preventDefault();
 }
 
 usernameForm.addEventListener('submit', connect, true); // step 1
